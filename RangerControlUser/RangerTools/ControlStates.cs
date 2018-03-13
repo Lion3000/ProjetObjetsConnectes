484676b1d3a50b117017ff522c652401cd94b922
Alex zarzitski <alex.zarzitski@gmail.com>
2018-03-13 11:14:59 +0100

    initialisation du projet VS et compte rendu

---
+++ RangerControlUser/RangerTools/ControlStates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/* ======================================================
+ * Auteur : Alex Zarzitski
+ * Date : 13/03/2018
+ */
+namespace RangerTools
+{
+    public enum ControlStates
+    {
+        RIGHT_TRACK_STOP,
+        RIGHT_TRACK_FORWARD,
+        RIGHT_TRACK_BACK,
+        LEFT_TRACK_STOP,
+        LEFT_TRACK_FORWARD,
+        LEFT_TRACK_BACK,
+        ARM_UP, ARM_DOWN,
+        ARM_STOP, GRIPPER_OPEN,
+        GRIPPER_CLOSE,
+        GRIPPER_STOP,
+        ALL_STOP
+    }
+}
