5ec0361e062fe33ea9d0dd8ecae6a1bdcb2b28ad
Alex zarzitski <alex.zarzitski@gmail.com>
2018-04-18 18:53:49 +0200

    bug fix

--- RangerControlUser/RangerTools/RangerControlRobot.cs
+++ RangerControlUser/RangerTools/RangerControlRobot.cs
@@ -8,23 +8,39 @@ using System.IO.Ports;
 
 namespace RangerTools
 {
+    /// <summary>
+    /// Cette classe controle le robot via l'enum ControlStates
+    /// </summary>
     public class RangerControlRobot
     {
         private SerialPort comPort;
 
-        public RangerControlRobot(string portName = "COM1", int baudRate = 9600)
+        /// <summary>
+        /// Le constructeur initialise la classe SerialPort avec le parametre portName
+        /// </summary>
+        /// <param name="portName">Cette parametre defini le port COM du robot</param>
+        public RangerControlRobot(string portName = "COM1")
         {
             this.comPort = new SerialPort();
             this.comPort.WriteBufferSize = 2;
-            this.comPort.BaudRate = baudRate;
+            this.comPort.Parity = Parity.Even;
+            this.comPort.StopBits = StopBits.One;
+            this.comPort.DataBits = 8;
+            this.comPort.BaudRate = 9600;
             this.comPort.PortName = portName;
         }
 
+        /// <summary>
+        /// Le deconstructeur deconnect le robot
+        /// </summary>
         ~RangerControlRobot()
         {
             this.disconnect();
         }
 
+        /// <summary>
+        /// Cette methoe tante d'etablere une comunication avec le robot
+        /// </summary>
         public void connect()
         {
             try
@@ -37,18 +53,23 @@ namespace RangerTools
             }
         }
 
+        /// <summary>
+        /// Cette methode deconnect le robot
+        /// </summary>
         public void disconnect()
         {
             this.comPort.Close();
         }
 
+        /// <summary>
+        /// Cette methode envoit les instructions à executé par le robot
+        /// </summary>
         public void setCommand(ControlStates state)
         {
             String command = ((int)state).ToString();
             if ((int)state < 10)
               command = "0" + command;
             this.comPort.Write(command);
-            Console.WriteLine("Read : "+this.comPort.ReadLine());
         }
     }
 }
--- RangerControlUser/UnitTestRangerTools/RangerControlRobotTest.cs
+++ RangerControlUser/UnitTestRangerTools/RangerControlRobotTest.cs
@@ -1,35 +1,53 @@
-﻿using RangerTools;
+﻿/* ======================================================
+ * Auteur : Alex Zarzitski
+ * Date : 18/04/2018
+ */
+ 
+ using RangerTools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
 
 namespace UnitTestRangerTools
 {
     [TestClass]
+    /// <summary>
+    /// Cette classe test le bon fonctionment du robot
+    /// </summary>
     public class RangerControlRobotTest
     {
+        /// <summary>
+        /// Cette methode test le robot. Une comunication est etablie avec le robot, 
+        /// puis les differets commandes sont envoie pour verifier le bon fonctionment des moteurs.
+        /// </summary>
         [TestMethod]
         public void TestOfControl()
         {
-            RangerControlRobot rangerControlRobot = new RangerControlRobot("COM13", 9600);
+            RangerControlRobot rangerControlRobot = new RangerControlRobot("COM13");
             rangerControlRobot.connect();
             rangerControlRobot.setCommand(ControlStates.TRACKS_FORWARD);
             Thread.Sleep(1000);
             rangerControlRobot.setCommand(ControlStates.TRACK_TURN_LEFT);
+            Thread.Sleep(3000);
+            rangerControlRobot.setCommand(ControlStates.TRACK_TURN_RIGHT);
+            Thread.Sleep(3000);
+            rangerControlRobot.setCommand(ControlStates.TRACKS_STOP);
+            Thread.Sleep(1000);
+            rangerControlRobot.setCommand(ControlStates.TRACKS_BACKWARD);
             Thread.Sleep(1000);
             rangerControlRobot.setCommand(ControlStates.TRACKS_STOP);
             Thread.Sleep(1000);
             rangerControlRobot.setCommand(ControlStates.ARM_UP);
-            Thread.Sleep(1000);
+            Thread.Sleep(5000);
             rangerControlRobot.setCommand(ControlStates.ARM_STOP);
             Thread.Sleep(1000);
             rangerControlRobot.setCommand(ControlStates.ARM_DOWN);
-            Thread.Sleep(1000);
+            Thread.Sleep(2000);
             rangerControlRobot.setCommand(ControlStates.ARM_STOP);
             Thread.Sleep(1000);
             rangerControlRobot.setCommand(ControlStates.GRIPPER_CLOSE);
-            Thread.Sleep(1000);
+            Thread.Sleep(2000);
             rangerControlRobot.setCommand(ControlStates.GRIPPER_OPEN);
-            Thread.Sleep(1000);
+            Thread.Sleep(2000);
             rangerControlRobot.setCommand(ControlStates.ALL_STOP);
             rangerControlRobot.disconnect();
         }
